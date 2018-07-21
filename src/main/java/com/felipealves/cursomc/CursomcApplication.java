1a0e912b931547540a7ec4234c4f4b0699defac3
Felipe Alves <felipeestudos.a.d.s@gmail.com>
2018-07-21 12:02:31 -0300

    Rest testando

--- src/main/java/com/felipealves/cursomc/CursomcApplication.java
+++ src/main/java/com/felipealves/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.felipealves;
+package com.felipealves.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
