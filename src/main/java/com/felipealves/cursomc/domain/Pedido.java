22152f7bf326ada1f98483974049626fbea69a48
Felipe Alves <felipeestudos.a.d.s@gmail.com>
2019-05-08 15:47:16 -0300

    ItemPedido e ItemPedidosPK

--- src/main/java/com/felipealves/cursomc/domain/Pedido.java
+++ src/main/java/com/felipealves/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.felipealves.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 
@@ -23,7 +26,6 @@ public class Pedido  implements Serializable {
 	private Integer id;
 	private Date instante;
 	
-	
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")			// Relacionamento um para um
 	private Pagamento pagamento;
 	
@@ -31,11 +33,12 @@ public class Pedido  implements Serializable {
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
 	
-	
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Integer getId() {
 		return id;
@@ -112,6 +115,13 @@ public class Pedido  implements Serializable {
 		return true;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 	
 }
 
