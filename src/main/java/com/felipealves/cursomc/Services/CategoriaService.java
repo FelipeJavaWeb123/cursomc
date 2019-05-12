4cd093179f9ce6d110f47580ce1a4320065e6dbb
Felipe Alves <felipeestudos.a.d.s@gmail.com>
2019-05-12 09:03:16 -0300

    Inserindo uma nova categoria com Post

--- src/main/java/com/felipealves/cursomc/Services/CategoriaService.java
+++ src/main/java/com/felipealves/cursomc/Services/CategoriaService.java
@@ -22,4 +22,9 @@ public class CategoriaService {
 				"Objeto não encontraddo! id: " + id + ", tipo: " + Categoria.class.getName()
 				));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
--- src/main/java/com/felipealves/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/felipealves/cursomc/resources/CategoriaResources.java
@@ -1,11 +1,16 @@
 package com.felipealves.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipealves.cursomc.Services.CategoriaService;
 import com.felipealves.cursomc.domain.Categoria;
@@ -15,7 +20,7 @@ import com.felipealves.cursomc.domain.Categoria;
 public class CategoriaResources {
 	
 	@Autowired
-	CategoriaService service;
+	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
@@ -23,4 +28,10 @@ public class CategoriaResources {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
