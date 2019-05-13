6f4d1933e3405f28e069d0297f44a4fa3f4f9ca4
Felipe Alves <felipeestudos.a.d.s@gmail.com>
2019-05-13 11:17:14 -0300

    Inserindo novo cliente com POST

--- src/main/java/com/felipealves/cursomc/Services/ClienteService.java
+++ src/main/java/com/felipealves/cursomc/Services/ClienteService.java
@@ -3,18 +3,22 @@ package com.felipealves.cursomc.Services;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.engine.jdbc.connections.internal.UserSuppliedConnectionProviderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.felipealves.cursomc.DTO.ClienteDTO;
+import com.felipealves.cursomc.DTO.ClienteNewDTO;
 import com.felipealves.cursomc.Exception.ObjectNotFoundException;
+import com.felipealves.cursomc.domain.Cidade;
 import com.felipealves.cursomc.domain.Cliente;
-import com.felipealves.cursomc.domain.Cliente;
+import com.felipealves.cursomc.domain.Endereco;
+import com.felipealves.cursomc.domain.Enum.TipoCliente;
 import com.felipealves.cursomc.repositores.ClienteRepository;
+import com.felipealves.cursomc.repositores.EnderecoRepository;
 
 @Service   
 public class ClienteService {
@@ -22,6 +26,8 @@ public class ClienteService {
 	@Autowired    
 	private ClienteRepository repo;
 	
+	@Autowired    
+	private EnderecoRepository repoEnd;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -30,8 +36,10 @@ public class ClienteService {
 				));
 	}
 	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
+		repoEnd.saveAll(obj.getEnderecos());
 		return repo.save(obj);
 	}
 	
@@ -64,4 +72,21 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()) );
+		Cidade cid = new Cidade(objDto.getCidadeId(),null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if (objDto.getTelefone2() != null) 
+			cli.getTelefones().add(objDto.getTelefone2());
+		
+		if (objDto.getTelefone3() != null)
+			cli.getTelefones().add(objDto.getTelefone3());
+		
+		return cli;
+	}
+	
 }
--- src/main/java/com/felipealves/cursomc/resources/ClienteResources.java
+++ src/main/java/com/felipealves/cursomc/resources/ClienteResources.java
@@ -1,5 +1,6 @@
 package com.felipealves.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,15 +12,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipealves.cursomc.DTO.ClienteDTO;
+import com.felipealves.cursomc.DTO.ClienteNewDTO;
 import com.felipealves.cursomc.Services.ClienteService;
+import com.felipealves.cursomc.domain.Categoria;
 import com.felipealves.cursomc.domain.Cliente;
 
 @RestController
@@ -29,6 +34,14 @@ public class ClienteResources {
 	@Autowired
 	ClienteService service;
 	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj =  service.find(id);
